1815f8555de3b455f62e149b307efc90e94155e2
estebancolhe <esteban.henao93@gmail.com>
2021-12-13 22:31:04 -0500

    configuracio del proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,36 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+# STATICFILES_DIR es la ruta donde se gurdaran los archivos estaticos
+# BASE_DIR es la ruta base configurada anteriormente el settings/base.py
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
